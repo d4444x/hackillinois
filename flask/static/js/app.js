c9972fa352e6f37da5eda1128983e66b82c374c1
Tayler <tayler.burns@gmail.com>
2014-04-12 04:08:15 -0400

    Added much, new trees

--- flask/static/js/app.js
+++ flask/static/js/app.js
@@ -1,1 +1,17 @@
 angular.module('tutsApp', []);
+
+$(document).ready(function() {
+  $('.tree > ul').attr('role', 'tree').find('ul').attr('role', 'group');
+  $('.tree').find('li:has(ul)').addClass('parent_li').attr('role', 'treeitem').find(' > span').on('click', function (e) {
+        var children = $(this).parent('li.parent_li').find(' > ul > li');
+        if (children.is(':visible')) {
+        children.hide('fast');
+        $(this).addClass('glyphicon-plus-sign').removeClass('glyphicon-minus-sign');
+        }
+        else {
+        children.show('fast');
+        $(this).addClass('glyphicon-minus-sign').removeClass('glyphicon-plus-sign');
+        }
+        e.stopPropagation();
+    });
+});
